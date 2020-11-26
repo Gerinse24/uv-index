21751005d782f40f074efa205cee259f5e42aae7
Geronimo <geronimo.shaw@gmail.com>
2020-11-26 13:36:37 +0800

    Now takes two command line arguments, sys.argv[1] is the state and sys.argv[2] is the city. This is limited to Australia. You can now run the scrupt as such: python uvindex.py <state> <city>

--- uvindex.py
+++ uvindex.py
@@ -6,12 +6,16 @@ import requests
 import re
 import sys
 
-# make request and store request object.
-r = requests.get("http://www.bom.gov.au/places/wa/perth/forecast/")
+# make request to url using two command line arguments.
+# sys.argv[1] is the state and sys.argv[2] is the city.
+r = requests.get("http://www.bom.gov.au/places/%s/%s/forecast/" % (sys.argv[1], sys.argv[2]))
+
 # searches r.text for any regex matches
 # findall returns a list of all matches which will be stored in result
 result = re.findall("Sun protection .*", r.text)
 
+# iterate the length of result. result[0] will be the index for today.
+# strip the remaining HTML code from result and print to screen.
 for i in range(len(result)):
     if i == 0:
         print("Today: %s" % result[i].strip("</p>   </div>"))
